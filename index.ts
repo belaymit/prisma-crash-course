6b3c5c8cc157c579b7d1d8f723414550c4f3e16a
belaymit <2belamit@gmail.com>
2024-09-12 13:05:45 +0300

    Added a logic to  retrieve embedded data

--- index.ts
+++ index.ts
@@ -22,17 +22,72 @@ async function main() {
     },
   ];
 
-  for (const continentData of continentsData) {
-    const continent = await prisma.continent.create({
-      data: {
-        name: continentData.name,
-        countries: {
-          create: continentData.countries, 
+  // for (const continentData of continentsData) {
+  //   const continent = await prisma.continent.create({
+  //     data: {
+  //       name: continentData.name,
+  //       countries: {
+  //         create: continentData.countries, 
+  //       },
+  //     },
+  //   });
+  //   console.log(`Created continent: ${continent.name}`);
+  // }
+
+  // const continents = await prisma.continent.findMany({
+  //   include: {
+  //     countries: true,
+  //   },
+  // });
+  // console.log(JSON.stringify(continents, null, 2));
+  const countries = await prisma.country.findMany();
+
+  const capitals: { [key: string]: string } = {
+    'Ethiopia': 'Addis Ababa',
+    'Kenya': 'Nairobi',
+    'South Africa': 'Pretoria',
+    'Germany': 'Berlin',
+    'France': 'Paris',
+    'Italy': 'Rome',
+    'China': 'Beijing',
+    'India': 'New Delhi',
+    'Japan': 'Tokyo',
+  };
+
+  // for (const country of countries) {
+  //   const capitalName = capitals[country.name];
+  //   if (capitalName) {
+  //     const existingCapital = await prisma.capitalCity.findUnique({
+  //       where: { countryId: country.id },
+  //     });
+
+  //     if (!existingCapital) {
+  //       await prisma.capitalCity.create({
+  //         data: {
+  //           name: capitalName,
+  //           countryId: country.id,
+  //         },
+  //       });
+  //       console.log(`Created capital ${capitalName} for country ${country.name}`);
+  //     } else {
+  //       console.log(`Capital already exists for country ${country.name}`);
+  //     }
+  //   } else {
+  //     console.log(`No capital defined for country ${country.name}`);
+  //   }
+  // }
+
+  const continent = await prisma.continent.findMany({
+    include: {
+      countries: {
+        include: {
+          capital: true,
         },
-      },
-    });
-    console.log(`Created continent: ${continent.name}`);
-  }
+      }
+    },
+  });
+
+  console.log("capitalsData", JSON.stringify(continent, null, 2));
 }
 
 main()
